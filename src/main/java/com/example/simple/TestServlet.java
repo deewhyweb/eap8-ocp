71bbf4b954d2a1ffef330a46d45e4bed9745aaff
Philip Hayes <philip@deewhy.ie>
2022-12-13 16:04:21 -0500

    initial commit

---
+++ src/main/java/com/example/simple/TestServlet.java
@@ -0,0 +1,16 @@
+package com.example.simple;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet(urlPatterns = "/hello")
+public class TestServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello World!");
+        writer.close();
+    }
+}
